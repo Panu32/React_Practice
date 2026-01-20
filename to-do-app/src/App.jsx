49bf12f0816675adf105a071507e83c1f776e71d
Pranav Vaidya <pranavvaidya2005@gmail.com>
2026-01-20 23:28:34 +0530

    useRef

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -1,28 +1,19 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import FirstComponent from './Components/firstComponent.jsx';
 // let x= "Pranav"
 // let arr= ["user1","user2","user3"]
 
 const App = () => {
+  
+  const [data,setData] = useState([])
+  const inputRef= useRef(null);
 
-  const [x, setx] = useState(0);
-  const btnClick = () => {
-    console.log("Click")
-    setx(x + 1)
-  }
-  let data = "Boy"
+  
   return (
     <div>
-      {/* {x}
-      {arr.map((user)=>{
-        return <h2>{user}</h2>
-      })} */}
-
-      {/* {data=="Boy" ? <h1>Boy</h1>: <h1> Girl</h1>} */}
-
-
-      <button onClick={() => { btnClick() }}>Click me</button>
-      <FirstComponent data={x} fn={setx} />
+      <input ref={inputRef} type="text" />
+      <button onClick={()=>{setData([...data,inputRef.current.value])}}> Submit </button>
+      {data.map((item,index)=>{return <h2 key= {index}>{item}</h2>})}
     </div>
   )
 }
